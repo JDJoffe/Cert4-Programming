d1dc40ebcf29f7dba6b3b4ac2e972ef17a9fc23d
Manny Vaccaro <emmanuel.vaccaro@gmail.com>
2019-05-28 12:19:23 +1000

    End of Week 14

---
+++ Group-A/Game-Systems/SunnyLand/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Prime31;
+
+public class Player : MonoBehaviour
+{
+    public float gravity = -10;
+    public float moveSpeed = 10f;
+
+    private CharacterController2D controller;
+    private Animator anim;
+   
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        Run(inputH);
+        Climb(inputV);
+    }
+
+    void Run(float inputH)
+    {
+        controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
+        anim.SetBool("IsRunning", inputH != 0);
+
+        // Sneak Peak:
+        // rend.flipX = inputH > 0
+    }
+
+    void Climb(float inputV)
+    {
+
+    }
+}
