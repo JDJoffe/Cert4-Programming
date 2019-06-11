082e6dc3d4218939949189762fbf81c91d4694de
Emmanuel Vaccaro <emmanuel.vaccaro@gmail.com>
2019-06-12 09:57:25 +1000

    Fixed GoHome

--- Artificial-Intelligence/GoHome/Assets/Scripts/Player.cs
+++ Artificial-Intelligence/GoHome/Assets/Scripts/Player.cs
@@ -26,24 +26,20 @@ public class Player : MonoBehaviour
             Cursor.visible = false;
         }    
     }
-
-    void FixedUpdate()
-    {
-        float inputR = Input.GetAxis("Mouse X");
-        Rotate(inputR);
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
+        // Get input from user
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
+        // Get character (cube) to move
         Move(inputH, inputV);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Did we hit an item?
+        // Did we hit a thing with an Item component?
         Item item = other.GetComponent<Item>();
         if (item)
         {
@@ -54,26 +50,22 @@ public class Player : MonoBehaviour
 
     public void Move(float inputH, float inputV)
     {
+        // 1. Copy rigid.velocity to 'velocity' vector
         Vector3 velocity = rigid.velocity;
-        Vector3 direction = transform.TransformDirection(new Vector3(inputH, 0, inputV)) * moveSpeed;
-        velocity = new Vector3(direction.x, velocity.y, direction.z);
+        // 2. Get input from user into 'inputDirection' vector
+        Vector3 inputDirection = new Vector3(inputH, 0, inputV);
+        // 3. Convert inputDirection from localspace to worldspace and store in 'direction' variable
+        Vector3 direction = transform.TransformDirection(inputDirection);
+        // 4. Set 'velocity' to Vector3(direction (x), velocity (y), direction (z)) and multiply by moveSpeed
+        velocity = new Vector3(direction.x, velocity.y, direction.z) * moveSpeed;
+        // 5. Apply new velocity to 'rigid.velocity' 
         rigid.velocity = velocity;
 
+        // Extra
         // Convert from World Space to Local Space via one axis
         //Transform camTransform = Camera.main.transform;
         //Vector3 camEuler = camTransform.eulerAngles;
         //// Localise the direction of input to the Camera Y angle (degrees)
         //velocity = Quaternion.Euler(0f, camEuler.y, 0f) * velocity; 
-    }
-    
-    public void Rotate(float inputR)
-    {
-        // Rotating using Rigidbody (using Physics)
-        float angle = inputR * rotationSpeed * Time.deltaTime;
-        Quaternion rotation = rigid.rotation * Quaternion.AngleAxis(angle, Vector3.up);
-        rigid.MoveRotation(rotation);
-
-        // Rotating object forcibly (using Translate) #nofilter
-        //transform.Rotate(Vector3.up, inputR * rotationSpeed * Time.deltaTime);
     }
 }
